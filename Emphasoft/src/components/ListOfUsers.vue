a2499c5b052e3b29466a4a28a72995b304858c43
Amil <Limash76912@mail.ru>
2020-07-29 13:58:03 +0300

    filter by username added

--- Emphasoft/src/components/ListOfUsers.vue
+++ Emphasoft/src/components/ListOfUsers.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="content_users_list">
-    <button class="content_users_list_logout" v-on:click="$emit('logOut')">Log Out</button>
+    <div class="content_users_list_header">
+      <button class="content_users_list_header_logout_btn" v-on:click="$emit('logOut')">Log Out</button>
+      <input
+        type="text"
+        class="content_users_list_header_logout_search"
+        placeholder="Search by Username..."
+        v-model="searchUser"
+      />
+    </div>
+
     <table class="list_table" border="1">
       <thead class="list_table_header">
         <tr>
           <td>
-            <div class="table_sort">
+            <div class="table_sort_id">
               <span class="list_table_id">ID</span>
               <button class="table_sort_btn" v-on:click="$emit('sortById')">
                 <img class="table_sort_icon" src="./../assets/icons8-sort-26.png" />
@@ -13,7 +22,7 @@
             </div>
           </td>
           <td>
-            <div class="table_sort">
+            <div class="table_sort_username">
               <span class="list_table_username">UserName</span>
               <button class="table_sort_btn" v-on:click="$emit('sortByUsername')">
                 <img class="table_sort_icon" src="./../assets/icons8-sort-26.png" />
@@ -29,11 +38,11 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="item of dataUsers" :key="item.index">
-          <th class="table_user_id">
+        <tr v-for="item of filteredList" :key="item.index">
+          <th>
             <span>{{item.id}}</span>
           </th>
-          <th class="table_user_username">
+          <th>
             <span>{{item.username}}</span>
           </th>
           <th>
@@ -55,7 +64,15 @@ export default {
     return {
       usernameSort: false,
       idSort: false,
+      searchUser: "",
     };
+  },
+  computed: {
+    filteredList() {
+      return this.dataUsers.filter((data) =>
+        data.username.toLowerCase().includes(this.searchUser.toLowerCase())
+      );
+    },
   },
   methods: {},
 };
@@ -69,8 +86,9 @@ export default {
   flex-direction: column;
   justify-content: center;
 }
-.content_users_list_logout {
+.content_users_list_header_logout_btn {
   width: 70px;
+  float: left;
   margin-bottom: 4px;
   background-color: #44475a;
   border-radius: 6%;
@@ -79,9 +97,31 @@ export default {
   padding: 3px 3px;
 }
 
-.content_users_list_logout:hover {
+.content_users_list_header_logout_btn:hover {
   background-color: #8be9fd;
   color: #44475a;
+}
+
+.content_users_list_header {
+  position: relative;
+  margin: 3px 4px;
+}
+
+.content_users_list_header_logout_search {
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%);
+  padding: 5px 20px;
+  color: #f8f8f8;
+  background-color: #44475a;
+  border: none;
+  border-radius: 5%;
+  font-size: 14px;
+}
+
+.content_users_list_header_logout_search:focus {
+  outline: none;
+  border: 1px solid #f8f8f8;
 }
 .table_sort_btn {
   width: 14px;
@@ -99,7 +139,12 @@ export default {
   height: 13px;
 }
 
-.table_sort {
+.table_sort_id {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.table_sort_username {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
